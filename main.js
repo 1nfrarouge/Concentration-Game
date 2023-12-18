537937b1d77c65389328a29a2228c32ca68d5077
1nfrarouge <samantha.yc.lim@gmail.com>
2023-12-18 18:47:04 -0500

    Initialize game variables and handle card click

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 /*----- constants -----*/
+const totalPairs = 6;
 
 /*----- state variables -----*/
+let pairsFound = 0;
+let cardsFlipped = 0;
+let isClickable = true;
+let wrongGuesses = 0;
 
 /*----- cached elements -----*/
 
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+
+// Card Click
+function handleCardClick(event) {
+    const card = event.target;
+    if (isClickable && card.classList.contains('card') && !card.classList.contains('flipped')) {
+        flipCard(card);
+    }
+}
+
+// Flip Card
+function flipCard(card) {
+    card.classList.add('flipped');
+}
