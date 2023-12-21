06a4cca1e6a951f02138d8464cbd788ce2f2c2aa
1nfrarouge <samantha.yc.lim@gmail.com>
2023-12-21 11:24:28 -0500

    Fix card flipping and game logic issues

--- js/main.js
+++ js/main.js
@@ -1,27 +1,26 @@
 /*----- constants -----*/
 const totalPairs = 6;
 const SOURCE_CARDS = [
-    {img: '', matched: false},
-    {img: '', matched: false},
-    {img: '', matched: false},
-    {img: '', matched: false},
-    {img: '', matched: false},
-    {img: '', matched: false},
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/BlueBerry.png?raw=true', matched: false },
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/BokChoy.png?raw=true', matched: false },
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/Cabbage.png?raw=true', matched: false },
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/Carrot.png?raw=true', matched: false },
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/Kale.png?raw=true', matched: false },
+    { img: 'https://github.com/1nfrarouge/Concentration-Game/blob/main/Strawberry.png?raw=true', matched: false },
 ];
-const CARD_BACK_IMAGE = '';
+const CARD_BACK_IMAGE = 'https://github.com/1nfrarouge/Concentration-Game/blob/main/Cloud.png?raw=true';
 
 /*----- state variables -----*/
-let pairsFound = 0;
-let cardsFlipped = 0;
+let shuffledCards;
+let pairsFound = 0; // Add this variable
+let firstCard = null;
 let isClickable = true;
 let wrongGuesses = 0;
-let shuffledCards;
 
 /*----- cached elements -----*/
 const board = document.querySelector('.board');
 const pairsFoundElement = document.getElementById('pairs-found');
 const wrongGuessesElement = document.getElementById('wrong-guesses');
-const timeRemainingElement = document.getElementById('time-remaining');
 
 /*----- event listeners -----*/
 board.addEventListener('click', handleCardClick);
@@ -29,49 +28,63 @@ board.addEventListener('click', handleCardClick);
 /*----- functions -----*/
 init();
 
-// Initilize the Game
+// Initialize the Game
 function init() {
     shuffledCards = generateShuffleCards();
     renderCards();
-
-    pairsFound = 0;
-    cardsFlipped = 0;
-    isClickable = true;
-    wrongGuesses = 0;
-
-    pairsFoundElement.textContent = pairsFound;
-    wrongGuessesElement.textContent = wrongGuesses;
+    resetGameStats();
+    console.log('Game initialize');
 }
+
 // Card Click
 function handleCardClick(event) {
-    const card = event.target;
-    if (isClickable && card.classList.contains('card') && !card.classList.contains('flipped')) {
-        flipCard(card);
+    if (!isClickable || event.target.className !== 'card') return;
+    const clickedCard = event.target;
+    
+    // Check if the card is already flipped or it's the same card
+    if (clickedCard.classList.contains('flipped') || clickedCard === firstCard) return;
+
+    flipCard(clickedCard);
+
+    if (!firstCard) {
+        firstCard = clickedCard;
+    } else {
+        checkForMatch(clickedCard, firstCard);
+        firstCard = null;
     }
 }
 
 // Flip Card
 function flipCard(card) {
-    const cardIndex = card.
-    card.classList.add('flipped');
-    cardsFlipped++;
-    if (cardsFlipped === 2) {
-        checkForMatch();
-    }
+    card.classList.toggle('flipped');
+    const cardIndex = parseInt(card.id.replace('card', ''));
+
+    // Get the correct image URL from the shuffledCards array
+    card.src = card.classList.contains('flipped') ? shuffledCards[cardIndex].img : CARD_BACK_IMAGE;
+    console.log('Card flipped');
 }
 
 // Check for Match
-function checkForMatch() {
-    const flippedCards = document.querySelectorAll('.flipped');
-    const [card1, card2] = flippedCards;
-    if (card1.textContent === card2.textContent) {
+function checkForMatch(card1, card2) {
+    const index1 = parseInt(card1.id.replace('card', ''));
+    const index2 = parseInt(card2.id.replace('card', ''));
+    if (index1 === index2) return; // Same Card clicked
+
+    if (shuffledCards[index1].img === shuffledCards[index2].img) {
         pairsFound++;
         pairsFoundElement.textContent = pairsFound;
-        checkForWin();
+        if (pairsFound === totalPairs) displayWinnerPopup();
     } else {
-        setTimeout
-
+        wrongGuesses++;
+        wrongGuessesElement.textContent = wrongGuesses;
+        isClickable = false;
+        setTimeout(() => {
+            flipCard(card1);
+            flipCard(card2);
+            isClickable = true;
+        }, 2000);
     }
+    console.log('Checking for match');
 }
 
 // Check for Win
@@ -79,45 +92,48 @@ function checkForWin() {
     if (pairsFound === totalPairs) {
         displayWinnerPopup();
     }
+    console.log('Check for win');
 }
 
 // Reset Game
-function resetGame() {
-    pairsFound = 0;
-    cardsFlipped = 0;
-    isClickable = 0;
-    wrongGuesses = 0;
-
+function resetGameStats() {
+    pairsFound = wrongGuesses = 0;
     pairsFoundElement.textContent = pairsFound;
     wrongGuessesElement.textContent = wrongGuesses;
-
-    resetBoard();
-
-    const popups = document.querySelectorAll('.pop-up')
-    popups.forEach((popup) => {
-        popup.style.display = 'none';
-    });
+    console.log('Reset game');
 }
 
 // Reset Board
 function resetBoard() {
     const cards = document.querySelectorAll('.card');
     cards.forEach((card) => {
-        card.classList.remove('.flipped')
+        card.classList.remove('flipped');
     });
-    const shuffledCards = generateShuffleCards();
-    renderCards(shuffledCards);
+    shuffledCards = generateShuffleCards(); // Update the global shuffledCards
+    renderCards();
+    console.log(shuffledCards);
 }
 
 // Shuffle Cards
 function generateShuffleCards() {
-    const symbols = [];
-    const allCards = symbols.concat(symbols);
+    let tempCards = [...SOURCE_CARDS, ...SOURCE_CARDS];
+    shuffleArray(tempCards);
+    return tempCards;
 }
 
 // Shuffle Array
+function shuffleArray(array) {
+    array.sort(() => Math.random() - 0.5);
+    console.log('Array shuffled');
+}
 
 // Render Cards on Board
-
-// Display Winner PopUp
-// Display Loser PopUp
+function renderCards() {
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach((card, index) => {
+        card.src = CARD_BACK_IMAGE;
+        card.classList.remove('flipped');
+        card.id = `card${index}`;
+    });
+    console.log('Rendering cards');
+}
